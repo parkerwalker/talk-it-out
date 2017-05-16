b65438edaf0d08694181c40844303fc98ac961e9
Ben Zeise <ben.zeise@gmail.com>
2017-05-16 11:53:58 -0500

    completed suite_1-1

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Talk It Out
 
+Purpose of assignment is to work as a team and finish the script.js!
+
+## Authors
+- Parker
+- Ben
+
 *See Prime assignment app for instructions. Then, update me!*
--- script.js
+++ script.js
@@ -1,37 +1,86 @@
-/*
-1. Start with the number 42 and set that equal to the variable `majorKey`
+// 1. Start with the number 42 and set that equal to the variable `majorKey`
+var majorKey = 42;
 
-2. Create conditional logic to check if `majorKey` is greater than or equal to 53
-2-1. If true, add 42 to `majorKey`
-2-2. If false, subtract 13 from `majorKey`
+// 2. Create conditional logic to check if `majorKey` is greater than or equal to 53
+// 2-1. If true, add 42 to `majorKey`
+// 2-2. If false, subtract 13 from `majorKey`
+if (majorKey >= 53) {
+  majorKey += 42;
+} else {
+  majorKey -= 13;
+}
 
-3. Add a string with a value of 11 to `majorKey`
+// 3. Add a string with a value of 11 to `majorKey`
+majorKey += '11';
+console.log('majorKey is' + majorKey);
 
-4. Create an array named `keyChars`, then loop through `majorKey` using the `charAt` method and set `keyChars[i]` to each value
+// 4. Create an array named `keyChars`, then loop through `majorKey` using the
+// `charAt` method and set `keyChars[i]` to each value
+var keyChars = [];
+for (var i=0; i < majorKey.length; i++) {
+  keyChars[i] = majorKey.charAt(i);
+}
+console.log(keyChars);
 
-5. Remove the first and last values from `keyChars`
+// 5. Remove the first and last values from `keyChars`
+keyChars.shift();
+keyChars.pop();
 
-6. Create a new variable. Loop **backwards** through `keyChars` and store each value into the new variable, combining each of the values of `keyChars`
+// 6. Create a new variable. Loop **backwards** through `keyChars` and store each
+// value into the new variable, combining each of the values of `keyChars`
+var reversedKeyChars = '';
+for (i=keyChars.length; i>0; i--) {
+  reversedKeyChars += keyChars[i-1];
+}
+console.log(reversedKeyChars);
 
-7. `parseInt` both the `majorKey` and the new variable created in Step 6, ensure that both `majorKey` and the new variable are set to these new parsed values.
+// 7. `parseInt` both the `majorKey` and the new variable created in Step 6,
+// ensure that both `majorKey` and the new variable are set to these new parsed values.
+majorKey = parseInt(majorKey);
+reversedKeyChars = parseInt(reversedKeyChars);
+console.log(reversedKeyChars);
+console.log('majorKey is' + majorKey);
 
-******* SWITCH PROGRAMMERS *******
+// ******* SWITCH PROGRAMMERS *******
 
-8. Add `majorKey` and the new variable created in Step 6 together and store them in `majorKey`
+// 8. Add `majorKey` and the new variable created in Step 6 together and store them in `majorKey`
+console.log(reversedKeyChars);
+majorKey += reversedKeyChars;
+// 9. If the new value of `majorKey` is less than 60, set `majorKey` equal to 14.
+// If not, check to see if it is equal to 2930, if it is, set `majorKey` equal to 27.
+// If neither of these are true, set `majorKey` to 2.
+if(majorKey < 60){
+  majorKey = 14;
+} if (majorKey === 2930){
+  majorKey = 27;
+}else {
+  majorKey = 2;
+}
+// 10. Create a while loop that counts down from 10 and increments `majorKey` by 1.
+var count = 10;
+while(count > 0){
+  majorKey++;
+  count--;
+}
+console.log('majorKey is' + majorKey);
+// 11. Create a function that accepts an argument `val`. The function should convert `val` to a string,
+// then drop the first character off the string, but only if there
+// is more than one character in the string. Return `val` and set `majorKey` to the returned value.
+function convertToStringAndMaybeDrop(val){
+  val = val.toString();
+  if(val.length > 1){
+    val = val.substring(1, val.length);
+  }
+  majorKey = val;
+  return val;
+}
+console.log('majorKey is' + majorKey);
 
-9. If the new value of `majorKey` is less than 60, set `majorKey` equal to 14. If not, check to see if it is equal to 2930, if it is, set `majorKey` equal to 27.
-If neither of these are true, set `majorKey` to 2.
+// 12. Call the function.
+  convertToStringAndMaybeDrop(majorKey);
+// 13. Console log `majorKey`.
+console.log('majorKey is' + majorKey);
+console.log(majorKey);
+// 14. Create an index.html file, link in this JavaScript file. Load in the browser and check the value from Step 13.
 
-10. Create a while loop that counts down from 10 and increments `majorKey` by 1.
-
-11. Create a function that accepts an argument `val`. The function should convert `val` to a string, then drop the first character off the string, but only if there
-is more than one character in the string. Return `val` and set `majorKey` to the returned value.
-
-12. Call the function.
-
-13. Console log `majorKey`. 
-
-14. Create an index.html file, link in this JavaScript file. Load in the browser and check the value from Step 13.
-
-15. Your answer should be a string value that equals 7. Is that what you got?
-*/
+// 15. Your answer should be a string value that equals 7. Is that what you got?
